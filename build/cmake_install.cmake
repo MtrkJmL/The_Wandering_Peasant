8071bafc12b4f637a9a0c704e921ef5a4377de67
MtrkJmL <mtrkjml@gmail.com>
2025-04-19 03:23:53 +0200

    game_name change

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/RPG_Game")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Wandering_Peasant")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
